44ae7be6325fa39e19b187e2c67a0d811006ab9c
David S <32343843+DavidS1998@users.noreply.github.com>
2021-12-13 22:59:42 +0100

    Delete include directory

--- include/submit.php
+++
@@ -1,9 +0,0 @@
-<?php include_once 'fetch.php';
-
-// Gets data from POST that is formatted by their named variables (see index.php)
-$productname = $_GET['productname'];
-
-// The current page is submit.php. 
-// The user should return to the previous page after submitting.
-header("Location: ./../index.php?search=$productname");
-?>
